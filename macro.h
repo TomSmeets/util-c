ee0568333e736ab41a47fe124ec5b1b97dc32e7a
Tom Smeets <tom@tsmeets.nl>
2018-05-14 21:53:04 +0200

    Fix STATIC_ASSERT error
    Use Counter to have unique variable names between headers.
    LINE could be the same in some headers.

--- macro.h
+++ macro.h
@@ -4,7 +4,7 @@
 #define STATIC_ASSERT4(COND,MSG) typedef char static_assertion_##MSG[(!!(COND))*2-1]
 #define STATIC_ASSERT3(X,L) STATIC_ASSERT4(X,static_assertion_at_line_##L)
 #define STATIC_ASSERT2(X,L) STATIC_ASSERT3(X,L)
-#define STATIC_ASSERT(X)    STATIC_ASSERT2(X,__LINE__)
+#define STATIC_ASSERT(X)    STATIC_ASSERT2(X, __COUNTER__)
 
 #define _packed __attribute__((packed))
 #define private static;
